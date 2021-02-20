5f1c3735fadbe77aa009a3be748d93d46f801c4a
rbdkqk <rbdkqk@gmail.com>
2021-02-20 23:33:30 +0900

    clone project init
      - `npx create-react-app --template redux`
      - `npm install -g yarn`
      - deleted some files / moved `counterSlice.js`
      - firebase init & created `firebase.js`

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdj_2AOoVc1On8iyIKzBEAnR903CQ54eg',
+  authDomain: 'slack-clone-5d115.firebaseapp.com',
+  projectId: 'slack-clone-5d115',
+  storageBucket: 'slack-clone-5d115.appspot.com',
+  messagingSenderId: '492222518950',
+  appId: '1:492222518950:web:64512302bf0249ea468667',
+};
