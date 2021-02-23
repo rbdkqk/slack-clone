9f361b8cd078cfdae62d0ae5688b5de5826b2f68
rbdkqk <rbdkqk@gmail.com>
2021-02-24 00:28:20 +0900

    SideBar 구현
      - `Sidebar.js` 컴포넌트, `SideBarOption.js` 컴포넌트
      - firebase 라이브러리 설치 및 웹상에서 설정 / `react firebase hooks` 설치 및 적용
      - redux 관련 수정 : `app/store.js`, `features/appSlice.js`

---
+++ src/components/SideBarOption.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { db } from '../firebase';
+import { enterRoom } from '../features/appSlice';
+
+export default function SideBarOption({ id, Icon, title, addChannelOption }) {
+  const dispatch = useDispatch();
+
+  const addChannel = () => {
+    const channelName = prompt('Please enter the channel name');
+    // if `channelName` has value, then...
+    if (channelName) {
+      db.collection('rooms').add({
+        name: channelName,
+      });
+    }
+  };
+
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
+
+  return (
+    <SideBarOptionContainer
+      onClick={addChannelOption ? addChannel : selectChannel}
+    >
+      {Icon && <Icon fontSize='small' style={{ padding: 10 }} />}
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SideBarOptionChannel>
+          <span>#</span> {title}
+        </SideBarOptionChannel>
+      )}
+    </SideBarOptionContainer>
+  );
+}
+
+const SideBarOptionContainer = styled.div`
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+
+  :hover {
+    opacity: 0.9;
+    background-color: #340e36;
+  }
+
+  > h3 {
+    font-weight: 500;
+  }
+
+  > h3 > span {
+    padding: 15px;
+  }
+`;
+
+const SideBarOptionChannel = styled.h3`
+  font-weight: 300;
+  padding: 10px 0;
+`;
