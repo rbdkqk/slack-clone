1d38242ff3338ba1b96de93b6709b7f6ca3bd050
rbdkqk <rbdkqk@gmail.com>
2021-02-23 22:22:18 +0900

    Header 부분의 형태 구현 : `Header.js`
      - 기능은 추가되어 있지 않고, css 및 아이콘 등만 구성함
      - 세부 컴포넌트를 더 나누지는 않았음

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,93 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 export default function Header() {
   return (
     <HeaderContainer>
-      {/* Header Left */}
       <HeaderLeft>
-        <HeaderAvatar />
+        <HeaderAvatar
+        // onClick={() =>}
+        />
+        <AccessTimeIcon />
       </HeaderLeft>
-      {/* Header Search */}
-      {/* Header Right */}
+
+      <HeaderSearch>
+        <SearchIcon />
+        <input placeholder='Search' />
+      </HeaderSearch>
+
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 }
 
 const HeaderContainer = styled.div`
-  color: red;
+  display: flex;
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
 `;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
-const HeaderAvatar = styled(Avatar)``;
+  // 아래 문법은 공부 필요
+  // 의도 : HeaderLeft 안에 있는 <AccessTimeIcon /> 컴포넌트는 아래 속성을 준다'
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
 
-const HeaderSearch = styled.div``;
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
 
-const HeaderRight = styled.div``;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
